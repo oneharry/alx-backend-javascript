64146f26b894e8fae0b6af4a582f835ff015c845
Oneharry <harryofficialchidi@yahoo.com>
2023-06-26 23:14:54 +0100

    fixing err in requirement

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     plugins: ['jest'],
     rules: {
+      'linebreak-style': 0,
       'no-console': 'off',
       'no-shadow': 'off',
       'no-restricted-syntax': [
