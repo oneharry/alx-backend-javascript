03f263d76892233d3d565fdc89c5c0202a3177de
Oneharry <harryofficialchidi@yahoo.com>
2023-06-28 00:27:37 +0100

    task 2 response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -8,6 +8,6 @@ export default function handleResponseFromAPI(promise) {
   })
     .catch(() => {
       console.log('Got a response from the API');
-      throw new Error();
+      return Error();
     });
 }
