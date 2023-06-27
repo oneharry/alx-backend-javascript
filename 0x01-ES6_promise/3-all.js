c84399c486de92bff30f787af53738534473e597
Oneharry <harryofficialchidi@yahoo.com>
2023-06-28 00:17:42 +0100

    task 2 response

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,8 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    }, () => {
+    })
+    .catch(() => {
       console.log('Signup system offline');
     });
 }
