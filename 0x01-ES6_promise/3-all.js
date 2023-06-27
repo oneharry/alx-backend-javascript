86eabc50546b74c50bef649b63d180233ec23bb5
Oneharry <harryofficialchidi@yahoo.com>
2023-06-27 22:13:22 +0100

    fixing task 2 fail

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,5 @@
-import { uploadPhoto, createUser } from '../utils.js';
+import uploadPhoto from '../utils.js';
+import createUser from '../utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto, createUser])
