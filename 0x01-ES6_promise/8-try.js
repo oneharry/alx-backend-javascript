b854a4eea67ac18ada782f5f93e4ab97c7cfda00
Oneharry <harryofficialchidi@yahoo.com>
2023-06-27 22:57:42 +0100

    fixing task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,8 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if(denominator == 0) {
-        throw new Error('cannot divide by 0');
-    } else {
-        return (numerator / denominator);
-    }
-  } catch (error) {
-    return error;
+  if(denominator === 0) {
+      throw new Error('cannot divide by 0');
+  } else {
+      return (numerator / denominator);
   }
 }
+
