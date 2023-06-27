caaa38dd8615d1d1ba0208efabbbc008f3c22812
Oneharry <harryofficialchidi@yahoo.com>
2023-06-27 23:36:26 +0100

    advanced task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-  Promise.allSettled(promises)
-  .then((result) => result);
+  return Promise.allSettled(promises);
 }
