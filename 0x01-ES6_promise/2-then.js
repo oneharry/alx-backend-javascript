7b9cb7e76f0c26de396468e087b1989c805271a3
Oneharry <harryofficialchidi@yahoo.com>
2023-06-27 22:25:03 +0100

    fixing task 2 fail

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-  .then(() => {
+  promise.then(() => {
     console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
   }, (error) => error);
 }
