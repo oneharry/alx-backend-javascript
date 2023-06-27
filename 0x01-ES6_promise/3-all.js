7efe6d232cd9ba72ccd4b7bfd0c8684685cec9a4
Oneharry <harryofficialchidi@yahoo.com>
2023-06-28 00:03:35 +0100

    fixed function callling in task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,7 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
+    }, () => {
       console.log('Signup system offline');
     });
 }
