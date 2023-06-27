f498d44dd3794c7596a02dda13ffb42ae384fac0
Oneharry <harryofficialchidi@yahoo.com>
2023-06-27 23:24:47 +0100

    advanced task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,10 @@
-import {uploadPhoto, createUser} from '../utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
   return {
     photo,
-    user
-  }
+    user,
+  };
 }
