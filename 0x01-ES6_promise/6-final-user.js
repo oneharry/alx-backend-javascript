56a3f9651f50bcb699ea509856f134b18da37aee
Oneharry <harryofficialchidi@yahoo.com>
2023-06-28 16:35:32 +0100

    fixed bug for task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled(promises)
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
     })))
     .catch((error) => error);
 }
