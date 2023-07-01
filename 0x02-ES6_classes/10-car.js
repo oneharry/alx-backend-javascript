9fdbc8c1a73947b46c2e71507fc1c0241c9b9151
Oneharry <harryofficialchidi@yahoo.com>
2023-07-01 03:08:24 +0100

    fixed bug task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,7 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    }
     this._brand = brand;
-
-    if (typeof motor !== 'string') {
-      throw new TypeError('motor must be a string');
-    }
     this._motor = motor;
-
-    if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
     this._color = color;
   }
 
@@ -21,7 +10,7 @@ export default class Car {
   }
 
   cloneCar() {
-    const Spec = this.constructor(Symbol.species);
+    const Spec = this.constructor[Symbol.species];
     return new Spec();
   }
 }
