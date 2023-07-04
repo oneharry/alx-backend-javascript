4863a0abc5e598a4c6fc83353fd00594f14dbce7
Oneharry <harryofficialchidi@yahoo.com>
2023-07-04 13:15:28 +0100

    added more function arguments checks

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startStrring) {
   const arr = [...set];
-  if (!startStrring) {
-    return startStrring;
+  if (!startStrring || !set || !(set instanceof Set)) {
+    return '';
   }
   const str = arr.filter((el) => (el.startsWith(startStrring)))
     .map((el) => el.slice(startStrring.length));
