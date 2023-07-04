b63ca08755d9dbb2bbbdeb0a2f4bf4fa5f327309
Oneharry <harryofficialchidi@yahoo.com>
2023-07-04 13:02:31 +0100

    fixed task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,20 @@
-export default function cleanSet(set, startStrring) {
-  const arr = [...set];
-  if (!startStrring) {
-    return '';
+// export default function cleanSet(set, startStrring) {
+//   const arr = [...set];
+//   if (!startStrring) {
+//     return '';
+//   }
+//   const str = arr.filter((el) => (el.startsWith(startStrring)))
+//     .map((el) => el.slice(startStrring.length));
+//   return str.join('-');
+// }
+
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return startString;
   }
-  const str = arr.filter((el) => (el.startsWith(startStrring)))
-    .map((el) => el.slice(startStrring.length));
-  return str.join('-');
+  const filteredValues = Array.from(set)
+    .filter((el) => el.startsWith(startString))
+    .map((el) => el.substring(startString.length));
+
+  return filteredValues.join('-');
 }
