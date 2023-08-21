23872628eca02638c41e309b80b8093f7d39e231
Oneharry <harryofficialchidi@yahoo.com>
2023-08-22 00:37:22 +0100

    printing to commandline

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ const rl =readline.createInterface({
     output: process.stdout
 })
 
-rl.question("Welcome to Holberton School, what is your name? ", (name) => {
+rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
     console.log(`Your name is: ${name}`);
 
     rl.close()
