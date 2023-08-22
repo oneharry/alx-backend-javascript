6916f578223d5cd9bc3820f0fc68a117e6a34a4c
Oneharry <harryofficialchidi@yahoo.com>
2023-08-22 23:34:06 +0100

    async readfile

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const countStudents = (path) => {
+
+    return new Promise((resolve, reject) => {
+        const data = fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error("Cannot load the database"));
+            }
+            const lines = data.split("\n");
+            const num = lines.length - 1;
+            const objs = {};
+            
+            for(let i=1; i < lines.length; i++) {
+                const val = lines[i].split(",");
+                const firstname = val[0];
+                const temp = val[3].replace(/\r/g, '')
+                if(objs[temp]) {
+                    objs[temp].push(firstname);
+                } else {
+                    objs[temp] = [firstname];
+                }
+            }
+            
+            console.log("Number of students: "+ num)
+            for(const obj in objs) {
+                const len = objs[obj].length;
+                const list = objs[obj].join(", ");
+                console.log(`Number of students in ${obj}: ${len}. List: ${list}`);
+            }
+            resolve(objs);
+        })
+    })
+
+}
+
+module.exports = countStudents;
